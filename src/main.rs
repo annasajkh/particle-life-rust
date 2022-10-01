b535be5a654ab603e8e638439fbaaf9ece73e488
annasajkh <annaskherunas06@gmail.com>
2022-10-01 18:19:59 +0700

    fix

---
+++ src/main.rs
@@ -0,0 +1,85 @@
+use macroquad::prelude::*;
+use particle::*;
+use macroquad::window;
+use std::f32::consts::PI;
+
+pub mod particle;
+
+
+fn window_conf() -> Conf {
+    Conf {
+        window_title: "Particle Life".to_owned(),
+        window_width: 768,
+        window_height: 488,
+        window_resizable: false,
+        ..Default::default()
+    }
+}
+
+fn init(particles: &mut [particle::Particle], particle_classes: &mut [particle::ParticleClass]) {
+    for i in 0..particle_classes.len() {
+        particle_classes[i] = ParticleClass::new();
+    }
+
+    for i in 0..particles.len() {
+        particles[i].position = Vec2::new(rand::gen_range(0.0, window::screen_width()), rand::gen_range(0.0, window::screen_height()));
+        particles[i].velocity = Vec2::from_angle(rand::gen_range(0.0, 2.0 * PI)).rotate(Vec2::new(20.0, 0.0));
+        particles[i].particle_class = particle_classes[(rand::rand() % (particle_classes.len() as u32)) as usize];
+    }
+}
+
+#[macroquad::main(window_conf)]
+async fn main() {
+    rand::srand(macroquad::miniquad::date::now() as _);
+
+    const COUNT: usize = 2000;
+    const CLASSES_COUNT: usize = 8;
+
+
+    let mut particles = [Particle::new(Vec2::new(0.0,0.0), Vec2::new(0.0, 0.0), ParticleClass::new()); COUNT];
+    let mut particle_classes = [ParticleClass::new(); CLASSES_COUNT];
+
+    init(&mut particles, &mut particle_classes);
+
+    loop {
+        clear_background(BLACK);
+
+        for i in 0..particles.len() {
+            let particle1 = &mut particles[i].clone();
+
+            for j in 0..particles.len() {
+                if i != j {
+                    let particle2 = &mut particles[j];
+    
+                    particle2.add_force(particle1);
+                }
+            }
+        }
+
+        for i in 0..particles.len() {
+            particles[i].update();
+        }
+
+        for i in 0..particles.len() {
+            let particle1 = &mut particles[i].clone();
+
+            for j in 0..particles.len() {
+                if i != j {
+                    let particle2 = &mut particles[j];
+
+                    particle2.resolve_collision(particle1);
+                }
+            }
+        }
+
+        for i in 0..particles.len() {
+            particles[i].draw();
+        }
+
+        if macroquad::input::is_key_pressed(KeyCode::Space)
+        {
+            init(&mut particles, &mut particle_classes);
+        }
+        next_frame().await
+    }
+}
---
+++ src/particle.rs
@@ -0,0 +1,114 @@
+use macroquad::{prelude::*, window};
+
+const RADIUS: f32 = 5.0;
+const FRICTION: f32 = 0.5;
+
+#[derive(Debug, Clone, Copy)]
+pub struct ParticleClass {
+    attract_force: f32,
+    repulse_force: f32,
+    max_radius: f32,
+    min_radius: f32,
+
+    color: Color,
+}
+
+impl ParticleClass {
+    pub fn new() -> Self {
+        
+        let max_radius = rand::gen_range(0.0, 50.0);
+
+        return Self {
+            attract_force: rand::gen_range(0.0, 200.0),
+            repulse_force: rand::gen_range(0.0, 200.0),
+            max_radius: max_radius,
+            min_radius: rand::gen_range(0.0, max_radius * 0.5),
+
+            color: Color::new(rand::gen_range(0.0, 1.0),
+                              rand::gen_range(0.0, 1.0),
+                              rand::gen_range(0.0, 1.0),
+                              1.0 ),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Particle {
+    pub position: Vec2,
+    pub velocity: Vec2,
+    pub particle_class: ParticleClass
+}
+
+impl Particle {
+    pub fn new(position: Vec2, velocity: Vec2, particle_class: ParticleClass) -> Self {
+        Self {
+            position: position,
+            velocity: velocity,
+            particle_class: particle_class
+        }
+    }
+
+    pub fn resolve_collision(&mut self, other: &mut Particle) {
+        let between = other.position - self.position;
+        let length2 : f32 = between.length_squared();
+    
+        if length2 - RADIUS * RADIUS * 4.0f32 < 0.0f32 {
+            let normal = between.normalize();
+    
+            let collision_depth = (length2.sqrt() - RADIUS * 2.0f32).abs() * 0.5f32;
+
+            self.position.x -= normal.x * collision_depth;
+            self.position.y -= normal.y * collision_depth;
+        }
+    }
+
+    pub fn add_force(&mut self, other: &mut Particle)
+	{
+        let distance2 = self.position.distance_squared(other.position);
+
+        if distance2 <= self.particle_class.max_radius * self.particle_class.max_radius && 
+           distance2 >= self.particle_class.min_radius * self.particle_class.min_radius {
+
+            let distance = distance2.sqrt();
+
+            let collision_dir_x = (self.position.x - other.position.x) / distance;
+            let collision_dir_y = (self.position.y - other.position.y) / distance;
+
+            self.velocity.x += collision_dir_x * self.particle_class.repulse_force;
+            self.velocity.y += collision_dir_y * self.particle_class.repulse_force;
+
+            if !(distance2 - RADIUS * RADIUS * 4.0f32 < 0.0f32) {
+                self.velocity.x -= collision_dir_x * self.particle_class.attract_force;
+                self.velocity.y -= collision_dir_y * self.particle_class.attract_force;
+
+            
+                // self.velocity.x += collision_dir_x * self.particle_class.attract_force;
+                // self.velocity.y += collision_dir_y * self.particle_class.attract_force;
+            }
+
+            self.velocity.x *= FRICTION;
+            self.velocity.y *= FRICTION;
+        }
+	}
+
+    pub fn update(&mut self) {
+        let delta = get_frame_time();
+
+        self.position.x += self.velocity.x * delta;
+        self.position.y += self.velocity.y * delta;
+
+        if self.position.x < -RADIUS {
+            self.position.x = window::screen_width() + RADIUS
+        } else if self.position.x > window::screen_width() + RADIUS {
+            self.position.x = -RADIUS
+        } else if self.position.y < -RADIUS {
+            self.position.y = window::screen_height() + RADIUS
+        } else if self.position.y > window::screen_height() + RADIUS {
+            self.position.y = -RADIUS
+        }
+    }
+
+    pub fn draw(&self) {
+        draw_circle(self.position.x, self.position.y, RADIUS, self.particle_class.color);
+    }
+}
